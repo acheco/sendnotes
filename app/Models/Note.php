64994a5cb1604adf242d7045833353eb341df9ad
Antony Checo <80710861+acheco@users.noreply.github.com>
2025-04-25 20:40:06 -0400

    Adding the Note model and migration

---
+++ app/Models/Note.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Note extends Model
+{
+  use HasFactory, HasUlids;
+
+  protected $fillable = [
+    'title',
+    'body',
+    'send_date',
+    'is_published',
+
+  ];
+
+}
